baae850038d868cef372f2022738cd1f9cbad740
CaviedesGitHub <l.padillac2@uniandes.edu.co>
2022-11-11 20:11:01 +0000

    workercloudstorage

--- conv_ms/run.sh
+++ conv_ms/run.sh
@@ -5,5 +5,5 @@ export FLASK_ENV=development
 sudo ufw allow 5003
 gunicorn --bind 0.0.0.0:5003 wsgi:flask_app
 
-celery -A app.celery_app beat
-celery -A app.celery_app worker -l info --pool=solo
+#celery -A app.celery_app beat
+#celery -A app.celery_app worker -l info --pool=solo
